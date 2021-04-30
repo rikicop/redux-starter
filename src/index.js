486aceb795507e342434477e71b6087de56f014c
rikicop <ruperto1@protonmail.com>
2021-04-30 17:56:55 -0400

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import {compose,pipe } from 'lodash/fp'
 
-let input = "javaScript  ";
-//let output = "<div>" + input.trim()+  "</div>";
+let input = "   JavaScript   ";
+
 const trim = str =>str.trim()
-const wrapInDiv = str => `<div>${str}</div>`;
+//Template String
+//const wrapInDiv = str => `<div>${str}</div>`;
+//const wrapInSpan = str => `<span>${str}</span>`;
+
+const wrap = type => str => `<${type}>${str}<${type}>`
+
 const toLowerCase = str => str.toLowerCase();
 
-const result = wrapInDiv(toLowerCase(trim(input)));
+//const result = wrapInDiv(toLowerCase(trim(input)));
 
-const transform = pipe(trim, wrapInDiv, toLowerCase);
-transform(input);
+const transform = pipe(trim,toLowerCase, wrap("span"));
+//transform(input);
 
 console.log(transform(input));
